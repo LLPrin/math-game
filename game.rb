c3bd0cddd55b1ddd123f9d7afbb66b95f7882fc2
LLPrin <LLPrin@users.noreply.github.com>
2016-11-11 06:50:44 +0000

    Files Created

---
+++ game.rb
@@ -0,0 +1,20 @@
+# --------------------------
+# |         GAME           |
+# --------------------------
+# | Properties             |
+# | ----------             |
+# | @player1               |
+# | @player2               |
+# | @is_running            | # Boolean. Start = True, End = False
+# |                        |
+# | Methods                |
+# | ----------             |
+# | play                   |
+# | turn_new               |
+# | input(str)             |
+# | output(str)            |
+# | generate_scores(str)   |
+# | turn_new               | # 5. Switch Turns, Loop ends
+# --------------------------
+# * @question # can be in turn or game, your decision
+# * @turn # your decision
